be37d9fb61947772952ffe850a18c30ee8e6752b
Filipe-Cumaru <facsa@cin.ufpe.br>
2018-08-01 11:02:07 -0300

    Update malha_hexaedros.cpp
    Correção na chamada do construtor moab::HomCoord.

--- malha_hexaedros.cpp
+++ malha_hexaedros.cpp
@@ -1,12 +1,13 @@
 /*
     Compilar: make malha_hexaedros
-    Executar: ./malha_hexaedros <número de hexaedros em x> <tamanho do hexaedro em x>
-                                <número de hexaedros em y> <tamanho do hexaedro em y>
+    Executar: ./malha_hexaedros <número de hexaedros em x> <tamanho do hexaedro em x> \
+                                <número de hexaedros em y> <tamanho do hexaedro em y> \
                                 <número de hexaedros em z> <tamanho do hexaedro em z>
 */
 
 #include <iostream>      /* std::cout */
 #include <cstdlib>       /* atoi, atof */
+#include <cmath>         /* ceil */
 #include "moab/Core.hpp"    /* moab::Core, moab::ErrorCode */
 #include "moab/ScdInterface.hpp"    /* moab::ScdInterface, moab::ScdBox */
 
@@ -17,24 +18,24 @@ int main(int argc, char *argv[]) {
     ErrorCode ret;
     Core mbcore;
     int num_hex_x_axis, num_hex_y_axis, num_hex_z_axis;
-    double size_hex_x_axis, size_hex_y_axis, size_hex_z_axis;
+    double dx, dy, dz;
 
     // Verificação da entrada e atribuição das variáveis.
     if (argc < 7) {
         num_hex_x_axis = 2;
-        size_hex_x_axis = 1.0;
+        dx = 0.1;
         num_hex_y_axis = 2;
-        size_hex_y_axis = 1.0;
+        dy = 0.1;
         num_hex_z_axis = 2;
-        size_hex_z_axis = 1.0;
+        dz = 0.1;
     }
     else {
         num_hex_x_axis = atoi(argv[1]);
-        size_hex_x_axis = atof(argv[2]);
+        dx = atof(argv[2]);
         num_hex_y_axis = atoi(argv[3]);
-        size_hex_y_axis = atof(argv[4]);
+        dy = atof(argv[4]);
         num_hex_z_axis = atoi(argv[5]);
-        size_hex_z_axis = atof(argv[6]);
+        dz = atof(argv[6]);
     }
 
     // Inicialização do vetor contendo as coordenadas dos vértices na malha.
@@ -46,9 +47,9 @@ int main(int argc, char *argv[]) {
     for (int k = 0; k < (num_hex_z_axis + 1); k++) {
         for (int j = 0; j < (num_hex_y_axis + 1); j++) {
             for (int i = 0; i < (num_hex_x_axis + 1); i++) {
-                vertex_coords[it] = i*size_hex_x_axis;
-                vertex_coords[it+1] = j*size_hex_y_axis;
-                vertex_coords[it+2] = k*size_hex_z_axis;
+                vertex_coords[it] = i*dx;
+                vertex_coords[it+1] = j*dy;
+                vertex_coords[it+2] = k*dz;
                 it += 3;
             }
         }
@@ -62,7 +63,7 @@ int main(int argc, char *argv[]) {
 
     ScdBox *structured_box = NULL;
     ret = structured_interface->construct_box(HomCoord(0.0, 0.0, 0.0),
-            HomCoord(num_hex_x_axis*size_hex_x_axis, num_hex_y_axis*size_hex_y_axis, num_hex_z_axis*size_hex_z_axis),
+            HomCoord(ceil(num_hex_x_axis*dx), ceil(num_hex_y_axis*dy), ceil(num_hex_z_axis*dz)),
             vertex_coords,
             NUM_VERTEX,
             structured_box);
@@ -72,7 +73,7 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < (num_hex_x_axis + 1); i++) {
         for (int j = 0; j < (num_hex_y_axis + 1); j++) {
             for (int k = 0; k < (num_hex_z_axis + 1); k++) {
-                cout << "Vertex (" << i*size_hex_x_axis << "," << j*size_hex_y_axis << "," << k*size_hex_z_axis << ") "
+                cout << "Vertex (" << i*dx << "," << j*dy << "," << k*dz << ") "
             		  << "has handle: " << structured_box->get_vertex(i,j,k) << endl;
             }
         }
